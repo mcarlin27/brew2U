bf4dc6f4889e46aefb3d15420815536775c8f8ff
Halle Tempest, James Lannon and Marilyn Carlin <james@>
2017-06-28 09:51:35 -0700

    initiate JS and HTML files

---
+++ js/beer-interface.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("#user-form").submit(function(event) {
+    event.preventDefault();
+    var currentUserObject = new User();
+    var location = $("#location").val();
+    var radius = $("#radius").val();
+    currentUserObject.getBeer(location, radius, displayBreweries);
+  });
+});
---
+++ js/beer.js
@@ -0,0 +1,12 @@
+var apiKey= require "90ac80c1a898af38e37a0a8ab7c195ca"
+
+
+
+function User() {
+  // this.location = location;
+  // this.radius = radius;
+}
+
+User.prototype.getBeer = function(location, radius, displayBreweries){
+
+}
