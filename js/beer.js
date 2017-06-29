a1ce7ceade5c5fe1625b24b7b747fcc0920ffe31
Halle Tempest, James Lannon and Marilyn Carlin <james@>
2017-06-28 17:26:28 -0700

    refactor backend to initialize map outside of AJAX.

--- js/beer.js
+++ js/beer.js
@@ -1,6 +1,6 @@
 var apiKey = require('./../.env').apiKey;
 var mapApiKey = require('./../.env').mapApiKey;
-
+var map;
 
 function User() {
 }
@@ -9,6 +9,20 @@ function LngLat(lng, lat) {
   this.lng = lng;
   this.lat = lat;
 }
+
+
+function initMap(lat, lng) {
+  var userInput = {
+    lat: lat,
+    lng: lng
+  };
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: userInput
+  });
+}
+
 
 var lngLatArray = [];
 
@@ -27,22 +41,16 @@ User.prototype.getBeer = function(location, displayBreweries) {
 
 
 LngLat.prototype.dropPin = function(lngLatArray, location) {
-  $.get('https://maps.googleapis.com/maps/api/js?key=' + mapApiKey + '&callback=initMap')
-    .then(function initMap() {
+  $.get('https://maps.googleapis.com/maps/api/js?key=' + mapApiKey)
+    .then(function(response) {
+      console.log(response);
       lngLatArray.forEach(function(lngLat) {
         var position = new google.maps.LatLng(lngLat.latitude, lngLat.longitude);
         new google.maps.Marker({
-          map: mapObject,
+          map: map,
           position: position
         });
       });
-
-      var myOptions = {
-        zoom : 16,
-        center : location,
-        mapTypeId : google.maps.MapTypeId.ROADMAP
-      };
-      var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
     });
   };
 
