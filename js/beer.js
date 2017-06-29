2299a8535455397d673791fd6d37df5fe5ed1ece
Halle Tempest, James Lannon and Marilyn Carlin <james@>
2017-06-29 13:30:50 -0700

    add functionality through if/else statements to replace undefined data with placeholder text.

--- js/beer.js
+++ js/beer.js
@@ -8,40 +8,85 @@ function LngLat(lng, lat) {
   this.lat = lat;
 }
 
+function Brewery(name, streetAddress, phone, website, hoursOfOperation) {
+  this.name = name;
+  this.streetAddress = streetAddress;
+  this.phone = phone;
+  this.website = website;
+  this.hoursOfOperation = hoursOfOperation;
+}
+
+var breweryArray = [];
 var lngLatArray = [];
 var locationArray = [];
 
 User.prototype.getBeerWithZip = function(location, displayBreweries) {
   $.get('http://api.brewerydb.com/v2/locations?key=' + apiKey + '&postalCode=' + location)
     .then(function(response) {
-
       response.data.forEach(function(element) {
-        displayBreweries('<li>' + element.name + '<br>' + element.streetAddress + '<br>' + element.phone + '<br>' + element.website + '<br>' + element.hoursOfOperation + '</li>');
-        var newLngLat = new LngLat(parseFloat(element.longitude), parseFloat(element.latitude));
-        lngLatArray.push(newLngLat);
-      });
+        var newBrewery = new Brewery(element.name, element.streetAddress, element.phone, element.website, element.hoursOfOperation);
+        breweryArray.push(newBrewery);
+
+        breweryArray.forEach(function(brewery) {
+          if (brewery.streetAddress != undefined) {
+            brewery.streetAddress = brewery.streetAddress;
+          } else {
+            brewery.streetAddress = "Street Address: N/A";
+          }
+          if (brewery.phone != undefined) {
+            brewery.phone = brewery.phone;
+          } else {
+            brewery.phone = "Phone: N/A";
+          }
+          if (brewery.website != undefined) {
+            brewery.website = brewery.website;
+          } else {
+            brewery.website = "Hours of Operation: N/A";
+          }
+          if (brewery.hoursOfOperation != undefined) {
+            brewery.hoursOfOperation = brewery.hoursOfOperation;
+          } else {
+            brewery.hoursOfOperation = "Website: N/A";
+          }
+          displayBreweries('<li>' + brewery.name + '<br>' + brewery.streetAddress + '<br>' + brewery.phone + '<br>' + brewery.website +  '<br>' + brewery.hoursOfOperation + '</li>');
+        });
     });
-    return lngLatArray;
-  };
+  });
+    // return lngLatArray;
+};
 
 User.prototype.getBeerWithCity = function(location, displayBreweries) {
   locationArray = location.split(", ");
   console.log(location);
   $.get('http://api.brewerydb.com/v2/locations?key=' + apiKey + '&locality=' + locationArray[0] + '&region=' + locationArray[1])
     .then(function(response) {
-      console.log(response);
       response.data.forEach(function(element) {
-        element.forEach(function(item) {
-          if (element.item !== undefined) {
-            return element.item;
-          } else {
-            return "No information available";
-          }
-        }); //THIS DOESN'T WORK
-        //WHAT ABOUT ZIPS WITH HYPHENS
-        //WHAT ABOUT NON-STANDARD ADDRESSES? WHAT ABOUT STATE ABBREVS?
+        var newBrewery = new Brewery(element.name, element.streetAddress, element.phone, element.website, element.hoursOfOperation);
+        breweryArray.push(newBrewery);
 
-        displayBreweries('<li>' + element.name + '<br>' + element.streetAddress + '<br>' + element.phone + '<br>' + element.website +  '<br>' + element.hoursOfOperation + '</li>');
+        breweryArray.forEach(function(brewery) {
+          if (brewery.streetAddress != undefined) {
+            brewery.streetAddress = brewery.streetAddress;
+          } else {
+            brewery.streetAddress = "Street Address: N/A";
+          }
+          if (brewery.phone != undefined) {
+            brewery.phone = brewery.phone;
+          } else {
+            brewery.phone = "Phone: N/A";
+          }
+          if (brewery.website != undefined) {
+            brewery.website = brewery.website;
+          } else {
+            brewery.website = "Hours of Operation: N/A";
+          }
+          if (brewery.hoursOfOperation != undefined) {
+            brewery.hoursOfOperation = brewery.hoursOfOperation;
+          } else {
+            brewery.hoursOfOperation = "Website: N/A";
+          }
+          displayBreweries('<li>' + brewery.name + '<br>' + brewery.streetAddress + '<br>' + brewery.phone + '<br>' + brewery.website +  '<br>' + brewery.hoursOfOperation + '</li>');
+        });
         // var newLngLat = new LngLat(parseFloat(element.longitude), parseFloat(element.latitude));
         // lngLatArray.push(newLngLat);
       });
